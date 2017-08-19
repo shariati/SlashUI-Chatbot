3138626cbc117e6f1720c0f045885b4da7b38546
Amin Shariati <amin.shariati@accenture.com>
2017-08-19 19:09:30 +0800

    Updated Facebook Token

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
+    FB_PAGE_TOKEN: 'EAABl3slnjIEBAEf0PLnZCFP1ONNZBy93XCGdoVnF218oPEV3MDZBd7L8QrptI1yK7ZAdap1oZAoc8GPnzlZAZAIXcaZB45wpoSTyrB9b7jG0A9pI8E1pkYFUTGytTrUaUZAaSit4w9dc0nmjJ1aZC5Jj0n69ja6x8NsxwsA8nx5S2evesxGfKfVBsQ',
+    FB_VERIFY_TOKEN: 'copper explain notice truck neat',
     API_AI_CLIENT_ACCESS_TOKEN: '',
     FB_APP_SECRET: '',
-    SERVER_URL: "",
+    SERVER_URL: 'https://slashui.herokuapp.com/webhook/',
 };
